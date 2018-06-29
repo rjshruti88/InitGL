1825fd0c1e3ba699e21b89548842fd9788e8798b
Shruti Ranganathan Jothi <sjothi@netflix.com>
2018-06-29 09:38:08 -0700

    set GLFW_OPENGL_ANY_PROFILE

--- source/main.cpp
+++ source/main.cpp
@@ -16,7 +16,7 @@ int main()
     glfwInit();
     glfwWindowHint(GLFW_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_VERSION_MINOR, 3);
-    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_OPENGL_ANY_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
     // glfw window creation
     // --------------------
