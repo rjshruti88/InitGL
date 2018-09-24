e86bfbb2ad8ffcf73ab522af1c259321cc0ef5a8
Shruti Ranganathan Jothi <sjothi@netflix.com>
2018-09-24 13:59:22 -0700

    correct binding

--- helloTriangle/source/triangle.cpp
+++ helloTriangle/source/triangle.cpp
@@ -123,14 +123,16 @@ int main()
          0.0f,  0.5f, 0.0f  // top
     };
 
-    unsigned int VAO, VBO;
+    unsigned int VAO;
     glGenVertexArrays(1, &VAO);
     glBindVertexArray(VAO);
 
-    glGenBuffers(1, &VBO);
     // copy our vertices array in a buffer for OpenGL to use
-    glBindBuffer(GL_ARRAY_BUFFER, VAO);
+    unsigned int VBO;
+    glGenBuffers(1, &VBO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
     // 1. then set the vertex attributes pointers
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
